a0575702af9605546f1e91b81dc96db4041f3984
mnisi-coder <59182644+mnisi-coder@users.noreply.github.com>
2021-06-14 21:24:27 +0200

    update

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,20 @@
 class Calculator{
     add(numbers){
 
+        if(numbers === '')
+        {
+            return 0;
+        }else{
+
+            // numbers = '1,2,2,5,25';
+            let arr = numbers.split(',');
+            
+            let sum = 0;
+            for (let i = 0; i < parseInt(arr.length); i++) {
+            sum += parseInt(arr[i]);
+            }
+            return sum ;
+        }
     }
 }
 
--- src/calulator.spec.js
+++ src/calulator.spec.js
@@ -6,5 +6,83 @@ describe("string calculator", function () {
         let calculator = new Calculator();
         let calculatorResult = calculator.add('');
         expect(calculatorResult).toBe(0);
+    });;
+
+    it("should return sum of an unknown numbers", function () {
+        let calculator = new Calculator();
+
+
+        let size = Math.floor(Math.random() * 11);
+    
+        let numberString= '';
+
+        for (let index = 0; index < parseInt(size); index++) {
+
+            let eletment = Math.floor(Math.random() * 11);
+            if(index === 0)
+            {
+                numberString += ''+eletment+'';
+            }else{
+                numberString += ','+eletment+'';
+            }
+            
+
+        }
+
+        let arr = numberString.split(',');
+
+
+    
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+        sum += parseInt(arr[i]);
+        }
+
+        let totalSum = arraySum;
+
+        let calculatorResult = calculator.add(numberString);
+        expect(calculatorResult).toBe(sum);
     });
 });
+
+
+function array()
+{
+    let size = Math.floor(Math.random() * 11);
+    
+    let numberString= '';
+
+    for (let index = 0; index < size; index++) {
+
+        let eletment = Math.floor(Math.random() * 11);
+        if(index === 0)
+        {
+            numberString += ''+eletment+'';
+        }else{
+            numberString += ','+eletment+'';
+        }
+        
+
+    }
+
+    
+
+    return numberString;
+    
+}
+
+
+
+function arraySum()
+{
+    let arr = array.split(',');
+
+
+    
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+    sum += parseInt(arr[i]);
+    }
+
+    return sum;
+}
